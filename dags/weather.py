9ffc8e3880d2e56fcf35e140f3c11c156f347719
Ihor K <ihork@Ihors-MacBook-Air.local>
2023-05-07 17:49:44 +0300

    first hw

--- dags/weather.py
+++ dags/weather.py
@@ -1,54 +1,57 @@
-from airflow import DAG
-from airflow.providers.sqlite.operators.sqlite import SqliteOperator
-from airflow.operators.http_operator import SimpleHttpOperator
-from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
-#from batch import pull_data
+import os
+import logging
+
 import requests
+from geopy.geocoders import Nominatim
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
 
 
-def pull_data():
-	url = "https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=d8169ca78476c40e04b22cdccd099fab"
+def get_city_coordinates(city_name: str):
+	logging.info('get_city_coordinates')
+	geolocator = Nominatim(user_agent='myapplication')
+	location = geolocator.geocode(city_name)
+	logging.info(location)
+	return location[1]
+
+def pull_weather_data(**kwargs):
+	logging.info("pull_data")
+	api_key = os.getenv('OPEN_WEATHER_API_KEY')
+	url = "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&appid={}"
 	payload={}
 	headers = {}
+	city_coordinates = get_city_coordinates(kwargs['city_name'])
 
-	response = requests.request("GET", url, headers=headers, data=payload)
-	return response.text
-
+	response = requests.request(
+		"GET", url.format(city_coordinates[0], city_coordinates[1], api_key), headers=headers, data=payload
+	)
+	json_obj = response.json() if response.ok else {}
+	temp = json_obj.get('current', {}).get('temp')
+	humidity = json_obj.get('current', {}).get('humidity')
+	clouds = json_obj.get('current', {}).get('clouds')
+	wind_speed = json_obj.get('current', {}).get('wind_speed')
+	logging.info(f'{temp}, {humidity}, {clouds}, {wind_speed}')
+	return temp, humidity, clouds, wind_speed
 
 
 with DAG(dag_id="weather", schedule_interval="@daily", start_date=days_ago(2)) as dag:
-	'''create_table_sqlite_task = SqliteOperator(
-		task_id="create_table_sqlite",
-		sqlite_conn_id="measurements_db",
-		sql=r"""
-		    CREATE TABLE IF NOT EXISTS measurements (
-			execution_time TIMESTAMP NOT NULL,
-			temperature FLOAT
-		);
-		""",
-	)'''
-	extract_data = SimpleHttpOperator(
-		http_conn_id="weather_api_con",
-		endpoint="data/2.5/weather",
-		data={"appid": "d8169ca78476c40e04b22cdccd099fab", "q": "Lviv"},
-		method="GET",
-		task_id="extract_data",
-		log_response=True
-	)
-	extract_data
-	#python_task1 = PythonOperator(task_id='python_task', python_callable=pull_data)
-	#python_task1
-	"""all_tasks = []
-	for counter in range(2):
-		python_task = PythonOperator(task_id=f'python_task_{counter}', python_callable=pull_data)
-		all_tasks.append(python_task)
-		python_task1 >> python_task
-	#python_task1 = PythonOperator(task_id='python_task1', python_callable=pull_data)
-	#python_task2 = PythonOperator(task_id='python_task2', python_callable=pull_data)
-	python_task3 = PythonOperator(task_id='python_task3', python_callable=pull_data)
-	
-	all_tasks >> python_task3"""
+	start = DummyOperator(task_id = 'start')
+	pull_operators = []
+	values = ['Lviv', 'Kyiv']
+	for counter, city_name in enumerate(values):
+		logging.info("@@@@@@@")
+		logging.info(counter)
+		logging.info(type(counter))
+		python_task = PythonOperator(
+			task_id=f'pull_providers_{counter + 1}',
+			python_callable=pull_weather_data,
+			op_kwargs={'city_name': city_name},
+		)
+		pull_operators.append(python_task)
+		start >> python_task
 
-
-	#requests.get("https://api.openweathermap.org/data/2.5/weather", data={"appid": "d8169ca78476c40e04b22cdccd099fab", "q": "Lviv"})
+	end = DummyOperator(task_id = 'end')
+	pull_operators >> end
