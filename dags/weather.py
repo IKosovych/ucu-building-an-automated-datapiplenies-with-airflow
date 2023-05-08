8e1461ddda1daafad5aee58b04fd12e4bd4db24a
Ihor K <ihork@192-168-0-102.stv.ntk>
2023-05-08 17:57:15 +0300

    add the possibility to fetch historical data

--- dags/weather.py
+++ dags/weather.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from datetime import datetime
 
 import requests
 from geopy.geocoders import Nominatim
@@ -20,13 +21,14 @@ def get_city_coordinates(city_name: str):
 def pull_weather_data(**kwargs):
 	logging.info("pull_data")
 	api_key = os.getenv('OPEN_WEATHER_API_KEY')
-	url = "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&appid={}"
+	url = "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&appid={}&dt={}"
 	payload={}
 	headers = {}
 	city_coordinates = get_city_coordinates(kwargs['city_name'])
+	timestamp = kwargs['timestamp']
 
 	response = requests.request(
-		"GET", url.format(city_coordinates[0], city_coordinates[1], api_key), headers=headers, data=payload
+		"GET", url.format(city_coordinates[0], city_coordinates[1], api_key, timestamp), headers=headers, data=payload
 	)
 	json_obj = response.json() if response.ok else {}
 	temp = json_obj.get('current', {}).get('temp')
@@ -40,12 +42,15 @@ def pull_weather_data(**kwargs):
 with DAG(dag_id="weather", schedule_interval="@daily", start_date=days_ago(2)) as dag:
 	start = DummyOperator(task_id = 'start')
 	pull_operators = []
+	dt = datetime.now()
+	# pass desired date
+	ts = datetime.timestamp(dt)
 	values = ['Lviv', 'Kyiv', 'Kharkiv', 'Odesa', 'Zhmerynka']
 	for counter, city_name in enumerate(values):
 		python_task = PythonOperator(
 			task_id=f'pull_providers_{str(counter + 1)}',
 			python_callable=pull_weather_data,
-			op_kwargs={'city_name': city_name},
+			op_kwargs={'city_name': city_name, 'timestamp': ts},
 		)
 		pull_operators.append(python_task)
 		start >> python_task
